9d0b0c5c29c8719f31590238627c3c742edff3b9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-04 10:15:04 +0300

    feat: example file reading added on startup

--- src/main/java/com/example/demoplugin/MyStartupActivity.java
+++ src/main/java/com/example/demoplugin/MyStartupActivity.java
@@ -2,11 +2,30 @@ package com.example.demoplugin;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.jetbrains.annotations.NotNull;
 
 public class MyStartupActivity implements StartupActivity {
   @Override
   public void runActivity(@NotNull Project project) {
     System.out.println("IntelliJ IDEA plugin is opening.");
+    String filePath = "D:\\test.txt";
+    readAndPrintFile(filePath);
+  }
+
+  private static void readAndPrintFile(String filePath) {
+    try {
+      Path path = Paths.get(filePath);
+      byte[] fileBytes = Files.readAllBytes(path);
+      String fileContent = new String(fileBytes, StandardCharsets.UTF_8);
+      System.out.println("File content:");
+      System.out.println(fileContent);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
