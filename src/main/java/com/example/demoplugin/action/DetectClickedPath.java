204a9aeb7f21b2edee44ff1f57d9f92a57fbc932
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-05 22:06:58 +0300

    feat: clicked path detection implemented

---
+++ src/main/java/com/example/demoplugin/action/DetectClickedPath.java
@@ -0,0 +1,24 @@
+package com.example.demoplugin.action;
+
+import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.vfs.VirtualFile;
+
+public class DetectClickedPath extends AnAction {
+
+  public DetectClickedPath() {
+    super("DetectClickedPath");
+  }
+
+  @Override
+  public void actionPerformed(AnActionEvent e) {
+    VirtualFile virtualFile = e.getDataContext().getData(CommonDataKeys.VIRTUAL_FILE);
+    if (virtualFile == null || !virtualFile.isDirectory()) {
+      System.out.println("err");
+      return;
+    }
+    String packagePath = virtualFile.getPath();
+    System.out.println("Clicked package folder path: " + packagePath);
+  }
+}
